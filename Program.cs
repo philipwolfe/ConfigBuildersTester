44693d019d914ea2692a424057751dac3472337f
Philip Wolfe <philip.wolfe@orion.com>
2022-05-31 22:38:49 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using ConfigBuildersTester.Properties;
+
+namespace ConfigBuildersTester
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Hello, {Settings.Default.Who}");
+            Console.ReadLine();
+        }
+    }
+}
